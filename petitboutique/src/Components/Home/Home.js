322af3a518b0310701d38e3404015d70f4bbc693
J. Diego Fernández <JDFBdeveloper@gmail.com>
2022-06-01 21:55:48 -0300

    Swiper and navbar

--- petitboutique/src/Components/Home/Home.js
+++ petitboutique/src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import SwiperMain from '../SwiperMain/SwiperMain';
 
 export default function Home() {
   return (
     <div>
         <Navbar/>
+        <SwiperMain/>
     </div>
   );
 }
--- petitboutique/src/Components/Navbar/Navbar.js
+++ petitboutique/src/Components/Navbar/Navbar.js
@@ -10,10 +10,20 @@ export default function Navbar() {
   return (
     <div className={s.container}>
         <img className={s.petitLogo} src={Logo} alt='Petit Boutique Logo'/>
-        <form className={s.form}>
-            <input className={s.input} placeholder='Encontra lo que buscas...'/>
-            <img className={s.lupa} src={Lupa} alt='Lupa'/>
-        </form>
+        <div className={s.search}>
+            <form className={s.form}>
+                <input className={s.input} placeholder='Encontra lo que buscas...'/>
+                <img className={s.lupa} src={Lupa} alt='Lupa'/>
+            </form>
+            <div className={s.categories}>
+                <button className={s.category}>Buzos</button>
+                <button className={s.category}>Remeras</button>
+                <button className={s.category}>Leggins</button>
+                <button className={s.category}>Vestidos</button>
+                <button className={s.category}>Gorritos</button>
+                <button className={s.category}>Zapatilas</button>
+            </div>
+        </div>
         <div className={s.logos}>
             <div className={s.logoContainer}>
                 <img className={s.logo} src={Insta} alt='Instagram Logo'/>
