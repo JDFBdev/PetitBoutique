9d9c16bec0ccd2b4280c9a3b9120ca24c391d72e
J. Diego Fernández <JDFBdeveloper@gmail.com>
2022-06-04 13:36:33 -0300

    Search Page

--- petitboutique/src/App.js
+++ petitboutique/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import Home from './Components/Home/Home';
+import Search from './Components/Search/Search';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <Home/>
+    <>
+      <Toaster/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/Search" element={<Search />}/>
+        </Routes>
+      </BrowserRouter>
+      </>
   );
 }
 
