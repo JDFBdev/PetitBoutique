72d589b4a0e30f0b38c45ea9426d2748e75de58d
J. Diego Fernández <JDFBdeveloper@gmail.com>
2022-06-16 11:28:10 -0300

    Form crear producto

--- petitboutique/src/Components/Card/Card.js
+++ petitboutique/src/Components/Card/Card.js
@@ -12,7 +12,7 @@ export default function Card({product, disableCart}){
         <div className={s.container}>
             <div className={s.imgContainer}>
             {
-                product.imagen ?
+                product ?
                 <img className={s.img} src={product.imagen[selected]} alt='Sin imagen'/> :
                 null
             }
@@ -21,21 +21,21 @@ export default function Card({product, disableCart}){
                 <div className={s.data}>
                     <div className={s.titleContainer}>
                     {
-                        product.nombre ? 
+                        product ? 
                         <h4 className={s.title}>{product.nombre}</h4>:
                         <h4 className={s.title}> </h4>
                     }
                     </div>  
                     <div className={s.priceContainer}>
                     {
-                        product.precio ?
+                        product ?
                         <p className={s.price}>${product.precio}</p>:
                         <p className={s.price}>$0</p>
                     }
                     <select className={s.talle}>
                         <option value='default' >Talle&nbsp;&nbsp;&nbsp;</option>
                         {
-                            product.talle[0] &&
+                            product &&
                             product.talle.map((o)=>{
                                 return <option value={o} key={o} >Talle {o}</option>
                             })
@@ -43,11 +43,11 @@ export default function Card({product, disableCart}){
                     </select>
                     </div>
                     {/* If colors > 4, justify-content: space-between, gap: .5rem */}
-                    <div className={s.colors} style={{justifyContent: product.color.length > 4 && 'space-between', gap: product.color.length > 4 && '.5rem' }}>
+                    <div className={s.colors} style={product && {justifyContent: product.color.length > 4 && 'space-between', gap: product.color.length > 4 && '.5rem' }}>
                     {
-                        product.color[0] ?
+                        product && product.color[0] ?
                         product.color.map((c,i)=>{
-                            return  <div id={i} className={s.color} style={{backgroundColor: c, transform: (selected === i) && 'scale(1.4)'}} onClick={handleColors}/>
+                            return  <div key={i} id={i} className={s.color} style={{backgroundColor: c, transform: (selected === Number(i)) && 'scale(1.4)'}} onClick={handleColors}/>
                         }): 
                         <div id={0} className={s.color} style={{backgroundColor: '#ffffff', transform: (selected === 0) && 'scale(1.4)'}} onClick={handleColors}/>
                     }
