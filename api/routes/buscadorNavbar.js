e49cf6dad766878ea005fae1ba99a57744fce346
santiset <sset@fi.uba.ar>
2022-07-04 15:25:41 -0300

    buscador navbar

--- api/routes/buscadorNavbar.js
+++ api/routes/buscadorNavbar.js
@@ -4,30 +4,53 @@ const Productos = require('../models/productos');
 const router = express.Router();
 
 router.get('/:param', async (req, res) => {
-    const {param} = req.params
+    const {param} = req.params;
+    const inputs = param.split(" ");
+    inputs.forEach(input => {
+        const index = inputs.indexOf(input);
+        if (input.length <= 3) {
+            inputs.splice(index, 1);
+        }
+    });
+    var productosNombre = [];
     const categorias = ["Pantalones", "Remeras", "Bebes", "Vestidos", "Accesorios", "Abrigos", "Nenes", "Nenas", "Unisex"];
-    const esCategoria = categorias.find(categoria => {
-        return param.toLowerCase() === categoria.toLowerCase();
-      });
+    if (inputs.length == 1) {
+        var esCategoria = categorias.find(categoria => {
+            return inputs[0].toLowerCase() === categoria.toLowerCase();
+        });
+    }
     try {
         if (esCategoria) {
             var productosCategoria = await Productos.findAll({
                 limit:8,
                 where: {
                     "categoria": {
-                        [Op.contains]: [param.toLowerCase()]
+                        [Op.contains]: [inputs[0].toLowerCase()]
                     }
                 }
             });
         } else {
-            var productosNombre = await Productos.findAll({
-                limit:8,
-                where: {
-                    "nombre": {
-                        [Op.iLike]: `%${param}%`
+            let promesas = [];
+            inputs.forEach(input => {
+                promesas.push( Productos.findAll({
+                    where: {
+                        "nombre": {
+                            [Op.iLike]: `%${input}%`
+                        }
                     }
-                }
+                }))
             });
+            await Promise.all(promesas).then(values => {
+                values.sort(function (a, b) {
+                    return a.length - b.length;
+                });
+                values.forEach(value => {
+                    productosNombre = productosNombre.concat(value);
+                })
+            })
+            if (productosNombre.length > 8) {
+                productosNombre.length = 8;
+            }
         }
     }
     catch(err) {
@@ -36,6 +59,12 @@ router.get('/:param', async (req, res) => {
     if (esCategoria) {
         return res.send(productosCategoria);
     }else if (productosNombre[0]) {
+        // filtro
+        productosNombre = productosNombre.filter((value, index, self) =>
+            index === self.findIndex((t) => (
+                t.id === value.id
+            ))
+        )
         return res.send(productosNombre);
     }
     res.send({message: "No se encontraron productos", success: true});
